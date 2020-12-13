e3639b0ccf0cfc5d33b6a71add4767ad0a8c7d76
Robi Irhamni <robidream20@gmail.com>
2020-12-13 14:33:04 +0700

    add user permission role

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,107 +1,107 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
+   <!-- Brand Logo -->
+   <a href="index3.html" class="brand-link">
+   <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+   <span class="brand-text font-weight-light">AdminLTE 3</span>
+   </a>
+   <!-- Sidebar -->
+   <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-        </div>
+         <div class="image">
+            <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+         </div>
+         <div class="info">
+            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+         </div>
       </div>
-
       <!-- SidebarSearch Form -->
       <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
+         <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+               <button class="btn btn-sidebar">
+               <i class="fas fa-search fa-fw"></i>
+               </button>
+            </div>
+         </div>
       </div>
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-            <li class="nav-item">
+
+            @if (auth()->user()->role == \App\User::ROLE_ADMIN || auth()->user()->role == \App\User::ROLE_USER)
+             <li class="nav-item">
                 <a href="{{ url('admin') }}" class="nav-link">
                     <i class="nav-icon fas fa-th"></i>
                     <p>
-                        Dashboard
+                    Dashboard
                     </p>
                 </a>
             </li>
+             @endif
+
             <li class="nav-item menu-open">
-                <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                    Data
-                    <i class="right fas fa-angle-left"></i>
-                </p>
-                </a>
-                <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{ url('admin/category') }}" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Category</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('products.index') }}" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Product</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('admin/transactions') }}" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Sales</p>
-                    </a>
-                </li>
-
-                <li class="nav-header">Sign Out</li>
-
-                <li class="nav-item">
-                    <a href="{{ route('logout') }}" class="nav-link">
+               <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>
+                     Data
+                     <i class="right fas fa-angle-left"></i>
+                  </p>
+               </a>
+               <ul class="nav nav-treeview">
+                @if (auth()->user()->role == \App\User::ROLE_ADMIN)
+                  <li class="nav-item">
+                     <a href="{{ url('admin/category') }}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Category</p>
+                     </a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="{{ route('products.index') }}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Product</p>
+                     </a>
+                  </li>
+                 @endif
+                 @if (auth()->user()->role == \App\User::ROLE_ADMIN || auth()->user()->role == \App\User::ROLE_USER)
+                  <li class="nav-item">
+                     <a href="{{ url('admin/transactions') }}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Sales</p>
+                     </a>
+                  </li>
+                  @endif
+                  <li class="nav-header">Sign Out</li>
+                  <li class="nav-item">
+                     <a href="{{ route('logout') }}" class="nav-link">
                         <i class="nav-icon far fa-circle text-danger"></i>
                         <p>
-                            Logout
+                           Logout
                         </p>
-                    </a>
-                </li>
-
-                </ul>
+                     </a>
+                  </li>
+               </ul>
             </li>
-        </ul>
+         </ul>
       </nav>
       <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  @push('scripts')
-     <script type="text/javascript">
-        $(function(){
-            var url = window.location;
-                $('ul.nav-sidebar a').filter(function() {
-                return this.href == url;
-            }).addClass('active');
-                $('ul.nav-treeview a').filter(function() {
-                return this.href == url;
-            }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open') .prev('a').addClass('active');
-        });
-      </script>
-  @endpush
-
+   </div>
+   <!-- /.sidebar -->
+</aside>
+@push('scripts')
+<script type="text/javascript">
+   $(function(){
+       var url = window.location;
+           $('ul.nav-sidebar a').filter(function() {
+           return this.href == url;
+       }).addClass('active');
+           $('ul.nav-treeview a').filter(function() {
+           return this.href == url;
+       }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open') .prev('a').addClass('active');
+   });
+</script>
+@endpush
